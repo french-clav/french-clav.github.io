43146f101022dabb5b2b713c451164cc6c5a4f45
Alexey Bykov <as.bykov@trueengineering.ru>
2023-01-19 04:39:23 +0700

    Implement modal displaying detail info on a composer, including name, birth, death, biography, photo

--- app/components/composerModal/Infobox.jsx
+++ app/components/composerModal/Infobox.jsx
@@ -5,6 +5,9 @@ import InfoboxEntry from "./InfoboxEntry.jsx"
 export default function Infobox({ composer }) {
     return (
         <div className="composer-modal-infobox">
+            <div className="composer-name">
+                {composer.name}
+            </div>
             <InfoboxEntry title="Рождение">
                 {composer.birth?.toString() ?? "?"}
             </InfoboxEntry>
