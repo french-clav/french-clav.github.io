2cdaf6721eb9962b614b426d94dedb3f3017c71e
Alexey Bykov <as.bykov@trueengineering.ru>
2023-01-27 01:55:44 +0700

    Remove succession diagram jerking on timeline becomes hidden

--- app/components/SuccessionDiagram.jsx
+++ app/components/SuccessionDiagram.jsx
@@ -2,6 +2,8 @@ import React from "react"
 
 export default function SuccessionDiagram() {
     return (
-        <img src="public/images/successionDiagram.png" style={{ maxWidth: "100%", maxHeight: "100%" }} />
+        <div className="zero-pos stretch xy-centerer">
+            <img src="public/images/successionDiagram.png" style={{ maxWidth: "100%", maxHeight: "100%" }} />
+        </div>
     )
 }
